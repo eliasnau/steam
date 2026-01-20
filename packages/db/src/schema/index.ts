6d64190ed1b6dfc57d3619f9c73f9d4c24955556
Elias Nau <contact@eliasnau.dev>
2026-01-20 21:32:17 +0100

    fix: db schema errors

--- packages/db/src/schema/index.ts
+++ packages/db/src/schema/index.ts
@@ -1,186 +1,208 @@
 export * from "./auth";
-import { relations } from "drizzle-orm";
+
+import { relations, sql } from "drizzle-orm";
 import {
-  pgTable,
-  text,
-  timestamp,
-  decimal,
-  date,
-  integer,
-  uuid,
-  primaryKey,
+	check,
+	date,
+	decimal,
+	integer,
+	pgTable,
+	primaryKey,
+	text,
+	timestamp,
+	uuid,
 } from "drizzle-orm/pg-core";
 
-export const game = pgTable("game", {
-  id: uuid("id").defaultRandom().primaryKey(),
+export const game = pgTable(
+	"game",
+	{
+		id: uuid("id").defaultRandom().primaryKey(),
 
-  steamId: text("steam_id").notNull(),
-  name: text("name").notNull(),
-  price: decimal("price").notNull(),
-  releasedAt: date("released_at").notNull(),
-  playerCountAllTime: integer("player_count_all_time").notNull(),
-  rating: text("rating").notNull(),
-  image: text("image"),
+		steamId: integer("steam_id").unique().notNull(),
+		name: text("name").notNull(),
+		price: decimal("price").notNull(),
+		releasedAt: date("released_at").notNull(),
+		playerCountAllTime: integer("player_count_all_time").notNull(),
+		rating: integer("rating").notNull(),
+		image: text("image"),
 
-  franchiseId: integer("franchise_id").notNull(),
+		franchiseId: uuid("franchise_id").references(() => franchises.id),
 
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-  updatedAt: timestamp("updated_at")
-    .defaultNow()
-    .$onUpdate(() => new Date())
-    .notNull(),
+		createdAt: timestamp("created_at").defaultNow().notNull(),
+		updatedAt: timestamp("updated_at")
+			.defaultNow()
+			.$onUpdate(() => new Date())
+			.notNull(),
+	},
+	(table) => [
+		check(
+			"rating_between_1_and_6",
+			sql`${table.rating} >= 1 AND ${table.rating} <= 6`,
+		),
+	],
+);
+
+export const franchises = pgTable("franchises", {
+	id: uuid("id").defaultRandom().primaryKey(),
+	name: text("name").notNull(),
+
+	createdAt: timestamp("created_at").defaultNow().notNull(),
+	updatedAt: timestamp("updated_at")
+		.defaultNow()
+		.$onUpdate(() => new Date())
+		.notNull(),
 });
 
 export const achievements = pgTable("achievements", {
-  id: uuid("id").defaultRandom().primaryKey(),
+	id: uuid("id").defaultRandom().primaryKey(),
 
-  name: text("name").notNull(),
-  description: text("description"),
+	name: text("name").notNull(),
+	description: text("description"),
 
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-  updatedAt: timestamp("updated_at")
-    .defaultNow()
-    .$onUpdate(() => new Date())
-    .notNull(),
+	createdAt: timestamp("created_at").defaultNow().notNull(),
+	updatedAt: timestamp("updated_at")
+		.defaultNow()
+		.$onUpdate(() => new Date())
+		.notNull(),
 });
 
 export const categories = pgTable("categories", {
-  id: uuid("id").defaultRandom().primaryKey(),
+	id: uuid("id").defaultRandom().primaryKey(),
 
-  name: text("name").notNull(),
+	name: text("name").notNull(),
 
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-  updatedAt: timestamp("updated_at")
-    .defaultNow()
-    .$onUpdate(() => new Date())
-    .notNull(),
+	createdAt: timestamp("created_at").defaultNow().notNull(),
+	updatedAt: timestamp("updated_at")
+		.defaultNow()
+		.$onUpdate(() => new Date())
+		.notNull(),
 });
 
 export const operatingSystems = pgTable("operating_systems", {
-  id: uuid("id").defaultRandom().primaryKey(),
+	id: uuid("id").defaultRandom().primaryKey(),
 
-  name: text("name").notNull(),
-  popularity: decimal("popularity").notNull(),
+	name: text("name").notNull(),
+	popularity: decimal("popularity").notNull(),
 
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-  updatedAt: timestamp("updated_at")
-    .defaultNow()
-    .$onUpdate(() => new Date())
-    .notNull(),
+	createdAt: timestamp("created_at").defaultNow().notNull(),
+	updatedAt: timestamp("updated_at")
+		.defaultNow()
+		.$onUpdate(() => new Date())
+		.notNull(),
 });
 
 export const genres = pgTable("genres", {
-  id: uuid("id").defaultRandom().primaryKey(),
+	id: uuid("id").defaultRandom().primaryKey(),
 
-  name: text("name").notNull(),
+	name: text("name").notNull(),
 
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-  updatedAt: timestamp("updated_at")
-    .defaultNow()
-    .$onUpdate(() => new Date())
-    .notNull(),
+	createdAt: timestamp("created_at").defaultNow().notNull(),
+	updatedAt: timestamp("updated_at")
+		.defaultNow()
+		.$onUpdate(() => new Date())
+		.notNull(),
 });
 
 export const publishers = pgTable("publishers", {
-  id: uuid("id").defaultRandom().primaryKey(),
+	id: uuid("id").defaultRandom().primaryKey(),
 
-  name: text("name").notNull(),
-  teamSize: text("team_size"),
+	name: text("name").notNull(),
+	teamSize: text("team_size"),
 
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-  updatedAt: timestamp("updated_at")
-    .defaultNow()
-    .$onUpdate(() => new Date())
-    .notNull(),
+	createdAt: timestamp("created_at").defaultNow().notNull(),
+	updatedAt: timestamp("updated_at")
+		.defaultNow()
+		.$onUpdate(() => new Date())
+		.notNull(),
 });
 
 export const developers = pgTable("developers", {
-  id: uuid("id").defaultRandom().primaryKey(),
+	id: uuid("id").defaultRandom().primaryKey(),
 
-  name: text("name").notNull(),
-  teamSize: text("team_size"),
+	name: text("name").notNull(),
+	teamSize: text("team_size"),
 
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-  updatedAt: timestamp("updated_at")
-    .defaultNow()
-    .$onUpdate(() => new Date())
-    .notNull(),
+	createdAt: timestamp("created_at").defaultNow().notNull(),
+	updatedAt: timestamp("updated_at")
+		.defaultNow()
+		.$onUpdate(() => new Date())
+		.notNull(),
 });
 
 export const gameToGenres = pgTable(
-  "game_to_genres",
-  {
-    gameId: uuid("game_id")
-      .notNull()
-      .references(() => game.id),
-    genreId: uuid("genre_id")
-      .notNull()
-      .references(() => genres.id),
-  },
-  (table) => [primaryKey({ columns: [table.gameId, table.genreId] })],
+	"game_to_genres",
+	{
+		gameId: uuid("game_id")
+			.notNull()
+			.references(() => game.id),
+		genreId: uuid("genre_id")
+			.notNull()
+			.references(() => genres.id),
+	},
+	(table) => [primaryKey({ columns: [table.gameId, table.genreId] })],
 );
 
 export const gameToCategories = pgTable(
-  "game_to_categories",
-  {
-    gameId: uuid("game_id")
-      .notNull()
-      .references(() => game.id),
-    categoryId: uuid("category_id")
-      .notNull()
-      .references(() => categories.id),
-  },
-  (table) => [primaryKey({ columns: [table.gameId, table.categoryId] })],
+	"game_to_categories",
+	{
+		gameId: uuid("game_id")
+			.notNull()
+			.references(() => game.id),
+		categoryId: uuid("category_id")
+			.notNull()
+			.references(() => categories.id),
+	},
+	(table) => [primaryKey({ columns: [table.gameId, table.categoryId] })],
 );
 
 export const gameToAchievements = pgTable(
-  "game_to_achievements",
-  {
-    gameId: uuid("game_id")
-      .notNull()
-      .references(() => game.id),
-    achievementId: uuid("achievement_id")
-      .notNull()
-      .references(() => achievements.id),
-  },
-  (table) => [primaryKey({ columns: [table.gameId, table.achievementId] })],
+	"game_to_achievements",
+	{
+		gameId: uuid("game_id")
+			.notNull()
+			.references(() => game.id),
+		achievementId: uuid("achievement_id")
+			.notNull()
+			.references(() => achievements.id),
+	},
+	(table) => [primaryKey({ columns: [table.gameId, table.achievementId] })],
 );
 
 export const gameToOperatingSystems = pgTable(
-  "game_to_operating_systems",
-  {
-    gameId: uuid("game_id")
-      .notNull()
-      .references(() => game.id),
-    operatingSystemId: uuid("operating_system_id")
-      .notNull()
-      .references(() => operatingSystems.id),
-  },
-  (table) => [primaryKey({ columns: [table.gameId, table.operatingSystemId] })],
+	"game_to_operating_systems",
+	{
+		gameId: uuid("game_id")
+			.notNull()
+			.references(() => game.id),
+		operatingSystemId: uuid("operating_system_id")
+			.notNull()
+			.references(() => operatingSystems.id),
+	},
+	(table) => [primaryKey({ columns: [table.gameId, table.operatingSystemId] })],
 );
 
 export const publishersToGame = pgTable(
-  "publisher_to_game",
-  {
-    gameId: uuid("game_id")
-      .notNull()
-      .references(() => game.id),
-    publisherId: uuid("publisher_id")
-      .notNull()
-      .references(() => publishers.id),
-  },
-  (table) => [primaryKey({ columns: [table.gameId, table.publisherId] })],
+	"publisher_to_game",
+	{
+		gameId: uuid("game_id")
+			.notNull()
+			.references(() => game.id),
+		publisherId: uuid("publisher_id")
+			.notNull()
+			.references(() => publishers.id),
+	},
+	(table) => [primaryKey({ columns: [table.gameId, table.publisherId] })],
 );
 
 export const gameToDevelopers = pgTable(
-  "game_to_developers",
-  {
-    gameId: uuid("game_id")
-      .notNull()
-      .references(() => game.id),
-    developerId: uuid("developer_id")
-      .notNull()
-      .references(() => developers.id),
-  },
-  (table) => [primaryKey({ columns: [table.gameId, table.developerId] })],
+	"game_to_developers",
+	{
+		gameId: uuid("game_id")
+			.notNull()
+			.references(() => game.id),
+		developerId: uuid("developer_id")
+			.notNull()
+			.references(() => developers.id),
+	},
+	(table) => [primaryKey({ columns: [table.gameId, table.developerId] })],
 );
