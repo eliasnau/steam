6868c65dc6235a13837b8d4dfa3d696837e38e9f
Elias Nau <contact@eliasnau.dev>
2026-01-20 19:17:11 +0100

    feat: add Access Control permissions

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -4,16 +4,27 @@ import { env } from "@repo/env/server";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { nextCookies } from "better-auth/next-js";
+import { admin as adminPlugin } from "better-auth/plugins"
+import { ac, admin, user, maintainer } from "./permissions"
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
-
     schema: schema,
   }),
   trustedOrigins: [env.CORS_ORIGIN],
   emailAndPassword: {
     enabled: true,
   },
-  plugins: [nextCookies()],
+  plugins: [
+    adminPlugin({
+      ac,
+      defaultRole: "user",
+      roles: {
+        admin,
+        // user,
+        maintainer
+      }
+    }),
+    nextCookies()],
 });
