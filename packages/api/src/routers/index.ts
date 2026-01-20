32d4ad38daaae528d9d6fdbcb41d8d742821e6f4
Elias Nau <contact@eliasnau.dev>
2026-01-20 22:55:03 +0100

    feat: add categories, franchises, genres and os endpoints

--- packages/api/src/routers/index.ts
+++ packages/api/src/routers/index.ts
@@ -1,11 +1,21 @@
 import type { RouterClient } from "@orpc/server";
 
 import { publicProcedure } from "../index";
+import { categoriesRouter } from "./categories";
+import { franchisesRouter } from "./franchises";
+import { gamesRouter } from "./games";
+import { genresRouter } from "./genres";
+import { operatingSystemsRouter } from "./operating-systems";
 
 export const appRouter = {
-  healthCheck: publicProcedure.handler(() => {
-    return "OK";
-  }),
+	healthCheck: publicProcedure.handler(() => {
+		return "OK";
+	}),
+	games: gamesRouter,
+	franchises: franchisesRouter,
+	genres: genresRouter,
+	categories: categoriesRouter,
+	operatingSystems: operatingSystemsRouter,
 };
 export type AppRouter = typeof appRouter;
 export type AppRouterClient = RouterClient<typeof appRouter>;
