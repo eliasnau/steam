2469cd7ad69b60faa54b4894d7b6192ea7067509
Elias <contact@eliasnau.dev>
2026-01-27 07:21:09 +0000

    fix: darkmode

--- apps/web/src/components/providers.tsx
+++ apps/web/src/components/providers.tsx
@@ -7,12 +7,17 @@ import { queryClient } from "@/utils/orpc";
 
 import { ThemeProvider } from "./theme-provider";
 import { Toaster } from "./ui/sonner";
+import { usePathname } from "next/navigation";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname()
+  const darkPages = ['/games']
+  const isDarkPage = darkPages.some(path => pathname.startsWith(path)) || pathname == "/"
+  const forcedTheme = isDarkPage ? 'dark' : undefined
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange forcedTheme={forcedTheme}>
       <QueryClientProvider client={queryClient}>
-      <NuqsAdapter>{children}</NuqsAdapter>
+        <NuqsAdapter>{children}</NuqsAdapter>
         <ReactQueryDevtools />
       </QueryClientProvider>
       <Toaster richColors />
